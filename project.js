ad19f22633845ce0bdbcd00707c85d4f241f744e
denharrison <denisharrison033@gmail.com>
2024-11-07 09:54:48 +0300

    Add gov post red

--- project.js
+++ project.js
@@ -1,15 +1,16 @@
  function game1() {
-    let usermeaning = Number(prompt('Попробуйте угадать число'));
-    const govmeaning = 73;
-
+    let usermeaning = Number(prompt('Попробуйте угадать число от 1 до 100'));
+    const govmeaning = Math.floor(Math.random() * 100) + 1;
     while (usermeaning !== govmeaning) {
         if (usermeaning < govmeaning) {
             alert("Неверно, загаданное число больше");
            } else if(usermeaning > govmeaning){
             alert('Неверно, загаданное число меньше');
            }
+           else if(usermeaning === govmeaning){
+            alert('Поздравляем! Вы угадали число!');
+           }
            else{
-            alert("Пожалуйста, введите число от 1 до 100.");
            }
          usermeaning = Number(prompt('Попробуйте угадать число'));
     }  
