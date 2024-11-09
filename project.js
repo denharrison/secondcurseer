a75c0a5cf93fc16a46ca8a7b7618b3bde9dea702
denharrison <denisharrison033@gmail.com>
2024-11-09 16:06:58 +0300

    Add neolit

--- project.js
+++ project.js
@@ -1,19 +1,12 @@
  function game1() {
     let userMeaning = Number(prompt('Попробуйте угадать число от 1 до 100'));
     const govMeaning = Math.floor(Math.random() * 100) + 1;
-   while (isNaN(userMeaning) || userMeaning < 1 || userMeaning > 100) {
-      userMeaning = Number(prompt('Ошибка! Введите число от 1 до 100:'));
-  }
   while (userMeaning !== govMeaning) {
     if (userMeaning < govMeaning) {
         alert("Неверно, загаданное число больше");
     } else {
         alert("Неверно, загаданное число меньше");
     } 
-    userMeaning = Number(prompt('Попробуйте угадать число от 1 до 100'));
-    while (isNaN(userMeaning) || userMeaning < 1 || userMeaning > 100) {
-     userMeaning = Number(prompt('Ошибка! Введите число от 1 до 100:'));
-   }
  }
  alert('Поздравляем! Вы угадали число!');
  }
