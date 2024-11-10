5af79031826c0179c644cdcdfa1f8123ba3c78f1
denharrison <denisharrison033@gmail.com>
2024-11-10 18:39:36 +0300

    Add redaction

--- project.js
+++ project.js
@@ -1,12 +1,12 @@
- function game1() {
-    let userMeaning = Number(prompt('Попробуйте угадать число от 1 до 100'));
-    const govMeaning = Math.floor(Math.random() * 100) + 1;
+function game1() {
+  let userMeaning = Number(prompt('Попробуйте угадать число от 1 до 100'));
+  const govMeaning = Math.floor(Math.random() * 100) + 1;
   while (userMeaning !== govMeaning) {
-    if (userMeaning < govMeaning) {
-        alert("Неверно, загаданное число больше");
-    } else {
-        alert("Неверно, загаданное число меньше");
-    } 
- }
- alert('Поздравляем! Вы угадали число!');
- }
+    if (userMeaning < govMeaning) { 
+        userMeaning = Number(prompt("Неверно, загаданное число больше"));
+    } else if (userMeaning > govMeaning) {
+        userMeaning = Number(prompt("Неверно, загаданное число меньше"));
+    }
+  }
+  alert('Поздравляем! Вы угадали число!'); 
+}
