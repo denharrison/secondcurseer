2997d45020d8d1e7913140371ca69530128cfe2f
denharrison <denisharrison033@gmail.com>
2024-11-13 14:42:51 +0300

    Add array hw

--- project.js
+++ project.js
@@ -10,3 +10,44 @@ function game1() {
   }
   alert('Поздравляем! Вы угадали число!'); 
 }
+function generateRandomTask() {
+  const operations = ['+', '-', '*', '/'];
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = Math.floor(Math.random() * 10) + 1;
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+  let task = '';
+  let correctAnswer;
+  switch (operation) {
+      case '+':
+          correctAnswer = num1 + num2;
+          task = `${num1} + ${num2}`;
+          break;
+
+      case '-':
+          correctAnswer = num1 - num2;
+          task = `${num1} - ${num2}`;
+          break;
+
+      case '*':
+          correctAnswer = num1 * num2;
+          task = `${num1} * ${num2}`;
+          break;
+
+      case '/':
+          correctAnswer = (num1 / num2).toFixed(2);
+          task = `${num1} / ${num2}`;
+          break;
+  }
+  return { task, correctAnswer };
+}
+
+function Game2() {
+  const { task, correctAnswer } = generateRandomTask();
+  const userAnswer = prompt(`Решите задачу: ${task}`);
+  if (parseFloat(userAnswer) === parseFloat(correctAnswer)) {
+      alert('Верный ответ!');
+  } else {
+      alert(`Неверно. Правильный ответ: ${correctAnswer}`);
+  }
+
+}
