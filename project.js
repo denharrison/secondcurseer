88fe61f6e435d546fa3febde8411aae950803f93
denharrison <denisharrison033@gmail.com>
2024-11-16 17:03:28 +0300

    Add hw6

--- project.js
+++ project.js
@@ -51,3 +51,43 @@ function Game2() {
   }
 
 }
+function Game3() {
+  const input = prompt('Введите текст, который будет перевернут !')
+  const reversed = input.split('').reverse().join('');
+  alert(reversed);
+}
+function Game4() {
+  const quiz = [
+
+    {
+        question: "Какого цвета небо?",
+        options: ["1. Красного", "2. Синего", "3. Зеленого"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "Сколько дней в неделе?",
+        options: ["1. Шесть", "2. Семь", "3. Восемь"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "Сколько у человека пальцев на одной руке?",
+        options: ["1. Четыре", "2. Пять", "3. Шесть"],
+        correctAnswer: 2
+    }
+
+];
+
+let score = 0;
+quiz.forEach((item) => {
+  let userAnswer = prompt(`${item.question}\ | ${item.options.join(' , ')}`);
+   if (parseInt(userAnswer) === item.correctAnswer) {
+  score++;
+  }
+
+});
+
+alert(`Вы правильно ответили на ${score} из ${quiz.length} вопросов.`);
+
+}
